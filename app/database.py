8cefb2155c073bae3fb8cf33e3a287a05736d7fd
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-10 23:25:58 +0900

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# SQLiteファイルに保存
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# connect_argsはSQLite固有の設定（マルチスレッド回避）
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# セッションの作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Baseクラス（モデル定義のベースになる）
+Base = declarative_base()
