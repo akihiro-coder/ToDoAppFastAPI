83445f85cae0eefdde8bf5ee96891aa5f792d925
akihirookuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-11 15:12:42 +0900

    詳細表示、更新、削除エンドポイントの関数作成と動作確認が完了

--- app/main.py
+++ app/main.py
@@ -33,3 +33,48 @@ def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=f'DB Error: {str(e)}')
+
+
+# 詳細取得
+@app.get("/todos/{todo_id}", response_model=schemas.Todo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Todoが見つかりませんでした。")
+    return todo
+
+# 更新
+@app.put("/todos/{todo_id}", response_model=schemas.Todo)
+def update_todo(todo_id: int, updated_todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Todoが見つかりません。")
+
+    # 値を上書き
+    todo.title = updated_todo.title
+    todo.description = updated_todo.description
+    todo.completed = updated_todo.completed
+
+    try:
+        db.commit()
+        db.refresh(todo)
+        return todo
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f'DB Error: {str(e)}')
+
+
+# 削除
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Todoが見つかりません。")
+
+    try:
+        db.delete(todo)
+        db.commit()
+        return {"message": "削除しました。"}
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f'DB Error: {str(e)}')
