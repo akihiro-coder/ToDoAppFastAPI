b4deec7f6eec1fd6668a4de45b9af3a934517e13
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-18 19:37:35 +0900

    update auth.py that user_id variable get int value

--- app/auth.py
+++ app/auth.py
@@ -47,13 +47,10 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: str = payload.get("sub")
+        user_id: int = int(payload.get("sub"))
         if user_id is None:
             raise credentials_exception
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            raise credentials_exception
+    except JWTError:
         raise credentials_exception
 
     user = db.query(models.User).filter(models.User.id == user_id).first()
